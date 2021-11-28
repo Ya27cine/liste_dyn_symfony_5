fc9b61cfb1d8f92d3b69f2ec1cf338cb66deff30
KHELIFA Yassine <prostam27@gmail.com>
2021-11-28 01:10:25 +0100

    fixtures data

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\City;
+use App\Entity\Country;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        
+       foreach ( range('A', 'F') as  $value) 
+        { 
+            $country = new Country();
+            $country->setName( $value );
+
+            for ($j=0; $j < rand(5,10); $j++) { 
+                $city = new City();
+                $city->setName( $country->getName()."#".$j);
+                $city->setCountry( $country );
+                $manager->persist($city);
+            }
+            $manager->persist($country);
+        }
+
+        $manager->flush();
+    }
+}
