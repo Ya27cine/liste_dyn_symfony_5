babf4bc52b7c77bad20dc06612cb84a52c2e9b13
KHELIFA Yassine <prostam27@gmail.com>
2021-11-28 11:59:33 +0100

    add field availabeAt

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,13 @@ use App\Repository\CityRepository;
 use App\Repository\CountryRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -63,6 +65,8 @@ class HomeController extends AbstractController
                 new NotBlank()
             ]
          ])
+
+         ->add('availableAt', DateTimeType::class)
          ->getForm()
         ;
 
